57ee5ed8a6510c8ffe83a87167853038b91f4c30
jiro141 <felipe.colmenaresvzla@gmail.com>
2025-07-29 09:39:32 -0400

    first commit

--- manage.py
+++ manage.py
@@ -2,11 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from pathlib import Path
+from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
+
+    # === Cargar variables de entorno desde .env ===
+    BASE_DIR = Path(__file__).resolve().parent
+    dotenv_path = BASE_DIR / '.env'
+    if dotenv_path.exists():
+        load_dotenv(dotenv_path)
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ia_assistant.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +24,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
