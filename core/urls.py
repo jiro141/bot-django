108bce702b3f7d26d899c95b6b474f7cd451067d
jiro141 <felipe.colmenaresvzla@gmail.com>
2025-07-29 09:29:11 -0400

    first commit

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('ask/', views.ask_ai),
-    path('text-to-speech/', views.text_to_speech),  # 👈 este debe existir
+    path('text-to-speech/', views.text_to_speech),  # 👈 ESTA ES LA QUE FALTA SEGURAMENTE
     path('audio-from-question/', views.get_audio_from_question),
     path('static-response/', views.create_static_response),
     path('last-audio-file-path/', views.get_last_audio_path),
